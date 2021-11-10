5eb5d16392f8ac88b64a19ae74d7fffb417f0421
CyberCadence <diljithv7@gmail.com>
2021-11-10 11:49:26 +0530

    Initial commit 1

---
+++ android/app/src/main/kotlin/com/example/bmi_own/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmi_own
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/input.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Container(color: Color(0xFF1D1E33),width: 170.0,height: 200.0,margin: EdgeInsets.all(15.0),)
+
+    );
+  }
+}
